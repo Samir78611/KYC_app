9260f161bf5bfbad6bc26026c476dcb860125e7c
Samir Qureshi <qureshisamar351@gmail.com>
2024-10-29 13:57:17 +0530

    ..

--- routes/api.php
+++ routes/api.php
@@ -110,5 +110,3 @@ Route::post('fssai-verification', [BusinessController::class, 'fssaiVerification
 Route::post('importExport-verification', [BusinessController::class, 'importExportVerification']);
 Route::post('gst-details', [BusinessController::class, 'verify']);
 
-
-//hii
