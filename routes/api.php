1d0b4d68d80cfec55dd4d35554ef558f4873401b
Nikita Deshmukh <deshmukhnikita003@gmail.com>
2024-11-04 13:33:56 +0530

    nikita chnages

--- routes/api.php
+++ routes/api.php
@@ -136,7 +136,5 @@ Route::post('digilocker-file', [DigilockerController::class, 'getDigiLockerFile'
 Route::post('digilocker-issued-file-list', [DigilockerController::class, 'getDigiLockerIssuedFileList']);
 //DigiLocker - Get self issued document
 Route::post('get-all-issued-documents', [DigilockerController::class, 'getAllIssuedDocuments']);
-
-
 //Legal Verification APIs
 Route::post('verify-court-record', [BusinessController::class, 'verifyCourtRecord']);
